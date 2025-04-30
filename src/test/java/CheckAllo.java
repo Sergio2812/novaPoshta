4f65b3bec8e73c7a79b782814c45ff31d77eac6a
Serhii Lebedynets <lebedynets88@ukr.net>
2025-04-30 20:30:17 +0300

    Add test to check Allo logo visibility and "hair dryer" search

--- src/test/java/CheckAllo.java
+++ src/test/java/CheckAllo.java
@@ -1,9 +1,16 @@
+import org.apache.commons.lang3.exception.ExceptionContext;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.time.Instant;
 
 public class CheckAllo {
 
@@ -20,5 +27,31 @@ public class CheckAllo {
         Assert.assertTrue(logo.isDisplayed(),"Логотип АЛЛО має бути видимим");
 
         driver.quit();
+    }
+    @Test
+    public void testAlloSearchForHairDryer(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://allo.ua/");
+
+        WebElement searchInput = driver.findElement(By.xpath("//input[@id='search-form__input']"));
+        Assert.assertTrue(searchInput.isDisplayed(), "Поле пошуку має бути видимим");
+
+        searchInput.sendKeys("фен");
+
+        WebElement searchButton = driver.findElement(By.xpath("//button[@class='search-form__submit-button']"));
+        searchButton.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement firsProduct =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='Фен Philips BHD342/10 серії 3000 ']")));
+
+        String productTitle = firsProduct.getText().toLowerCase();
+        Assert.assertTrue(productTitle.contains("фен"), "Назва першого товару має містити слово 'фен'");
+
+        driver.quit();
+
+
+
     }
 }
